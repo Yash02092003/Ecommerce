59810226800501860ce50d5b999f412bd81c2c00
Yash02092003 <mg.anujtyagi1@gmail.com>
2024-05-27 20:15:36 +0530

    Nothing

---
+++ models/user.js
@@ -0,0 +1,32 @@
+const { Role } = require('appwrite');
+const { ref } = require('joi');
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+const Product = require('./product');
+
+
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    role : {
+        type : String ,
+        default : 'buyer'
+    } ,
+    cart : [
+        {
+            type : mongoose.Schema.Types.ObjectId ,
+            ref : 'Product'
+        }
+    ]
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
